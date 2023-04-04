52cf7352ecddb54c3a2e7cc8ae38808820946b1a
AmrEl-Qattan <AmrElQattan95@gmail.com>
2023-04-04 19:53:37 +0200

    update6

--- src/index.js
+++ src/index.js
@@ -23,6 +23,7 @@ const routes = createBrowserRouter ([
       {path:"register", element : <Register/>},
       {path:"cart", element : <Cart/>},
       {path:"products", element : <Products/>},
+      {path:"ecommerce", element : <Home/>},
       {path:"*", element : <NotFound/>},
     ],
   },
